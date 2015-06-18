090b3b5051932774881cc760a9bdd2b165ac8db3
Quentin Curtet <quentin.curtet.pro@gmail.com>
2015-06-18 10:24:59 +0200

    Add interfaces

---
+++ Entity/EventInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Symposium\Bundle\EventBundle\Entity;
+
+/**
+ * EventInterface.
+ */
+interface EventInterface
+{
+    //Implémenter les méthodes
+}
---
+++ Entity/PayableItemInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Symposium\Bundle\EventBundle\Entity;
+
+/**
+ * PayableItemInterface
+ */
+interface PayableItemInterface
+{
+    //Implémenter les méthodes
+}
